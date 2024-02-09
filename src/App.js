d8e1d4a4b77fc6a14fae7c65848d65378f7ab9d8
David Petrovski <petrovskidavid1@gmail.com>
2024-02-09 01:53:43 -0600

    Fixed router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Question from "./Pages/Question/QuestionPage";
 import Celebration from "./Pages/Celebration/Celebration";
@@ -6,12 +6,12 @@ import Celebration from "./Pages/Celebration/Celebration";
 export default function App() {
   return (
     <div className="app">
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route exact path="/" element={<Question />} />
           <Route exact path="/i-love-you" element={<Celebration />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
